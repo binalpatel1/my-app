300c47905dea9901e0ffbf8f7eadb54b3eb2ea8e
John Doe <johndoe@example.com>
2021-08-15 15:54:46 -0700

    adding  arrow function in app.js

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,20 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
-function creatNotes(notes){
-return(
-<Note title={notes.title}
-content={notes.content}/>
 
-);
-
-}
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(creatNotes)}
+      {notes.map((notes)=> (
+      <Note title={notes.title}
+      content={notes.content}/>
+
+
+    ))}
       <Footer />
     </div>
   );
